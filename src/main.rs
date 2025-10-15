1d00d6b80f7b09fd060591be263622e2ff159806
garbagememo <66831242+garbagememo@users.noreply.github.com>
2025-10-15 15:40:24 +0900

    第5章カメラ実装

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-mod raymod;
+﻿mod raymod;
 use raymod::*;
 
 use rayon::prelude::*;
@@ -6,8 +6,7 @@ use rayon::prelude::*;
 use std::io::Write;
 
 fn ray_color(ray: &Ray) -> Color {
-    let u_d=ray.d.norm();
-    let t=0.5*(u_d.y+1.0);
+    let t=0.5*(ray.d.y+1.0);
     Vec3::new(1.0,1.0,1.0)*(1.0-t)+Vec3::new(0.5,0.7,1.0)*t
 }
         
@@ -25,24 +24,26 @@ fn main() {
 
     let v_h=2.0;
     let v_w=aspect*v_h;
-    let v_l=1.0;
+    let f_l=1.0;
     let origin=Vec3::new(0.0,0.0,0.0);
     let horizontal=Vec3::new(v_w,0.0,0.0);
     let vertical=Vec3::new(0.0,v_h,0.0);
-    let llc=origin-horizontal/2.0-vertical/2.0-Vec3::new(0.0,0.0,v_l);
+    let llc=origin-horizontal/2.0-vertical/2.0-Vec3::new(0.0,0.0,f_l);
 
 
     let bands: Vec<(usize, &mut [Color])> = image.chunks_mut(w as usize).enumerate().collect();
     bands.into_par_iter().for_each(|(y, band)| {
-      for x in 0..w {
-          let u=x as f64/(w as f64);
-          let v=y as f64/(h as f64);
+        for x in 0..w {
+            let u=x as f64/(w as f64);
+            let v=y as f64/(h as f64);
           
-          let ray=Ray::new(origin,llc+horizontal*u+vertical*v-origin);
-          let col=ray_color(&ray);
-          println!("{:?}",col);
-          band[x as usize] = col;
-      }
+            let ray=Ray::new(origin,llc+horizontal*u+vertical*v-origin);
+            band[x as usize] = ray_color(&ray);
+        }
+        if (y % 20)==0 {
+            print!("y={0}  :",y);
+            println!("col={:?}",band[0]);
+        };
     });
 
     //    save_ppm_file("image.ppm", image, w, h);
