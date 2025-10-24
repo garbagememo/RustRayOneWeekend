7106ca5c8a0df29932b3888cab65d05f6f713fd1
garbagememo <66831242+garbagememo@users.noreply.github.com>
2025-10-24 16:11:15 +0900

    第8章まで

--- src/main.rs
+++ src/main.rs
@@ -5,14 +5,18 @@ use rayon::prelude::*;
 
 use std::io::Write;
 
-fn ray_color(r: &Ray,world:&dyn Shape) -> Vec3 {
-    let hit_info=world.hit(&r,0.0,f64::MAX);
+fn ray_color(r: &Ray,world:&dyn Shape,depth:i64) -> Vec3 {
+	if depth <= 0 {
+        return Vec3::new(0.0, 0.0, 0.0);
+    }
+    let hit_info=world.hit(&r,EPS,f64::MAX);
     if let Some(hit)=hit_info {
-        (hit.n+Vec3::new(1.0,1.0,1.0) )*0.5
+        let target =hit.p+hit.n+Vec3::random_hemisphere();
+        ray_color(&Ray::new(hit.p,target-hit.p),world,depth-1)*0.5
     } else {
         let t=0.5*(r.d.norm().y+1.0);
         Vec3::new(1.0,1.0,1.0)*(1.0-t)+Vec3::new(0.5,0.7,1.0)*t
-   }
+    }
 }
         
 
@@ -24,6 +28,7 @@ fn main() {
     let aspect=16.0/9.0;
     let w: usize = 384;
     let h: usize = ((w as f64)/aspect) as usize;
+    let samps:usize = 128;
 
     let mut image = vec![Color::zero(); (w * h) as usize];
 
@@ -34,6 +39,7 @@ fn main() {
     let horizontal=Vec3::new(v_w,0.0,0.0);
     let vertical=Vec3::new(0.0,v_h,0.0);
     let luc=origin-horizontal/2.0+vertical/2.0-Vec3::new(0.0,0.0,f_l);
+	let MAX_DEPTH:i64=32;
 
     let mut world = ShapeList::new();
     world.push(Box::new(Sphere::new(Vec3::new(0.0, 0.0, -1.0), 0.5)));
@@ -43,11 +49,15 @@ fn main() {
     let bands: Vec<(usize, &mut [Color])> = image.chunks_mut(w as usize).enumerate().collect();
     bands.into_par_iter().for_each(|(y, band)| {
         for x in 0..w {
-            let u=x as f64/(w as f64);
-            let v=y as f64/(h as f64);
-          
-            let ray=Ray::new(origin,luc+horizontal*u-vertical*v-origin);
-            band[x as usize] = ray_color(&ray,&world);
+            let mut r = Vec3::new(0.0,0.0,0.0);
+            for spp in 0..samps {
+                let u=(x as f64 + random() ) /(w as f64);
+                let v=(y as f64 + random() ) /(h as f64);
+                
+                let ray=Ray::new(origin,luc+horizontal*u-vertical*v-origin);
+                r = r +ray_color(&ray,&world,MAX_DEPTH)/(samps as f64);
+            }
+            band[x as usize] = r; 
         }
         if (y % 20)==0 {
             print!("y={0}  :",y);
