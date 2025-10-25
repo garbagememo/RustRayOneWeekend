d74ad0613473ba0a4e1a829489d27ebbdcc94f34
garbagememo <66831242+garbagememo@users.noreply.github.com>
2025-10-25 23:04:45 +0900

    第11章

--- src/raymod/rayunit.rs
+++ src/raymod/rayunit.rs
@@ -25,6 +25,47 @@ pub enum Refl {
     Refr,
 }
 
+//左上が原点なPNGフォーマット対応
+pub struct Camera {
+    pub origin: Vec3,
+    pub upper_left_corner: Vec3,
+    pub horizontal: Vec3,
+    pub vertical: Vec3,
+}
+
+impl Camera {
+   pub fn new(lookfrom: Vec3, lookat: Vec3, vup: Vec3, vfov: f64, aspect_ratio: f64) -> Camera {
+        let theta = vfov.to_radians();
+        let h = (theta / 2.0).tan();
+        let viewport_height = 2.0 * h;
+        let viewport_width = aspect_ratio * viewport_height;
+
+        let w = (lookfrom - lookat).norm();
+        let u = ((vup % w)).norm();
+        let v = w % u;
+
+       let origin = lookfrom;
+       let horizontal =  u * viewport_width;
+       let vertical = v * viewport_height ;
+       let upper_left_corner = origin - horizontal / 2.0 + vertical / 2.0 - w;
+
+        Camera {
+            origin,
+            upper_left_corner,
+            horizontal,
+            vertical,
+        }
+    }
+
+    pub fn get_ray(&self, u: f64, v: f64) -> Ray {
+        Ray::new(
+            self.origin,
+			self.upper_left_corner + self.horizontal * u -  self.vertical * v - self.origin,
+
+        )
+    }
+}
+
 pub struct HitInfo {
     pub t: f64,
     pub p: Vec3,
